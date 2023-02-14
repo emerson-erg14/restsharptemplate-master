fb8852db456ed404eaac3f3486cfc425e54c3f5e
emerson-erg14 <125379203+emerson-erg14@users.noreply.github.com>
2023-02-14 16:20:17 -0300

    Adicionando o template

---
+++ restsharptemplate-master/RestSharpTemplate/Requests/PetStore/AddPetRequest.cs
@@ -0,0 +1,30 @@
+﻿using RestSharp;
+using RestSharpTemplate.Bases;
+using RestSharpTemplate.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpTemplate.Requests.PetStore
+{
+    public class AddPetRequest : RequestBase
+    {
+
+        public AddPetRequest()
+        {
+            url = "https://petstore.swagger.io/v2";
+            requestService = "/pet";
+            method = Method.POST;
+        }
+
+        public void setJsonBody()
+        {
+            jsonBody = File.ReadAllText(GeneralHelpers.ReturnProjectPath() + "Jsons/AddPetRequest.json", Encoding.UTF8);
+        }
+
+
+    }
+}
---
+++ restsharptemplate-master/RestSharpTemplate/Tests/PetTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using RestSharpTemplate.Bases;
+using RestSharpTemplate.Requests.PetStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpTemplate.Tests
+{
+    [TestFixture]
+    public class PetTests : TestBase
+    {
+
+        [Test]
+        public void AdicionarPetTest()
+        {
+            string statusCodeEsperado = "OK";
+
+            AddPetRequest addPetRequest = new AddPetRequest();
+            addPetRequest.setJsonBody();
+            IRestResponse<dynamic> response = addPetRequest.ExecuteRequest();
+
+            Assert.AreEqual(statusCodeEsperado, response.StatusCode.ToString());
+        }
+
+
+
+    }
+}
